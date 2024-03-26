a878efe1d3e731391698b4b49298df1716a7fa4e
HossamAhmedGitHub <hossam_a_ali64@yahoo.com>
2024-03-26 17:34:19 +0200

    refactored TC_login_01

--- src/test/java/com/nopcommerce/demo/testcases/LoginTest.java
+++ src/test/java/com/nopcommerce/demo/testcases/LoginTest.java
@@ -3,9 +3,6 @@ package com.nopcommerce.demo.testcases;
 import com.nopcommerce.demo.base.BaseTestCases;
 import com.nopcommerce.demo.pages.HomePage;
 import com.nopcommerce.demo.pages.LoginPage;
-
-import static com.nopcommerce.demo.utilities.ConfigUtliities.createInstance;
-
 import com.nopcommerce.demo.utilities.User;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -13,10 +10,9 @@ import org.testng.annotations.Test;
 public class LoginTest extends BaseTestCases {
 
     @Test(description = "verify login _FUNC with valid email and password")
-    public void TC_addToCart_00()
+    public void TC_login_01()
     {
-        LoginPage loginObject = new LoginPage(driver);
-        boolean status = loginObject
+        boolean status = new LoginPage(driver)
                 .load()
                 .login(User.getEmail(), User.getPassword())
                 .loginVerification();
